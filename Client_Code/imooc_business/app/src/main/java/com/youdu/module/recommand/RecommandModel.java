770e96defa0ea152af1db0375a343cbf4dde6405
stevenwg <1305676305@qq.com>
2018-05-06 17:11:40 +0800

    客户端最终完成
    完成

---
+++ Client_Code/imooc_business/app/src/main/java/com/youdu/module/recommand/RecommandModel.java
@@ -0,0 +1,13 @@
+package com.youdu.module.recommand;
+
+import com.youdu.module.BaseModel;
+
+import java.util.ArrayList;
+
+
+public class RecommandModel extends BaseModel {
+
+    public ArrayList<RecommandBodyValue> list;
+    public RecommandHeadValue head;
+
+}
